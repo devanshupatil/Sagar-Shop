97e32fac9c7fb6dcee40bcc105de8c7f3adf33ec
devanshupatil <devanshupatil692@gmail.com>
2025-01-25 19:07:16 +0530

    Backend

---
+++ Backend/index.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const app = express()
+const dotenv = require('dotenv');
+// const supabase = require('./config/db');
+const port = 4000;
+
+const cors = require('cors');
+
+
+// load environment variables from.env file
+dotenv.config();
+
+
+// Connect to Supabase
+// (async () => {
+//   try {
+//     const { data, error } = await supabase.from('products').select('*');
+//     if (error) throw error;
+//     console.log('Successfully connected to Supabase');
+//   } catch (error) {
+//     console.error('Error connecting to Supabase:', error.message);
+//   }
+// })();
+
+
+app.use(cors({
+  origin: [
+    'http://localhost:4001',  // Local development
+    'http://localhost:5173',
+    'https://wishlist-manager-app.netlify.app',  // Production frontend
+    'https://wishlist-manager-application.onrender.com'  // If needed
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
+
+app.options('*', cors());
+
+
+// // Middleware
+app.use(express.json());
+
+// // Routes
+// app.use('/api', productRouter);
+
+// Start server
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
