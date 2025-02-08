be414ed05b3e986c390fe45b04769eb4820fdaf9
devanshupatil <devanshupatil692@gmail.com>
2025-02-08 21:31:58 +0530

    Product Routes

---
+++ Backend/routes/productRoute.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+// const verifyToken  = require('../middleware/auth');
+const { getProducts, getProductById, checkProductStock } = require('../controller/productController');
+
+
+// router.use(verifyToken);
+
+router.get('/products', getProducts);
+
+router.get('/products/:id', getProductById);
+
+router.get('/products/stock/:id', checkProductStock);
+
+module.exports = router;
+
+
