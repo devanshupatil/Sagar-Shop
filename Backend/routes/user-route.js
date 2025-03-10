c71287981a95cd2c100c2cb0cb06d44e5707d7f9
devanshupatil <devanshupatil692@gmail.com>
2025-03-10 20:06:33 +0530

    update

--- Backend/routes/user-route.js
+++ Backend/routes/user-route.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { saveUserShippingDetails, getUserShippingDetails, updateUserShippingDetails, getUserById } = require('../controller/user-controller');
+const verifyToken  = require('../middleware/auth');
+
+
+router.use(verifyToken);
 
 router.post('/users/shipping-details', saveUserShippingDetails);
 router.get('/users/shipping-details/:userId', getUserShippingDetails);
