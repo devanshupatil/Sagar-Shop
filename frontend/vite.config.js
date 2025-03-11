d860fa0df59ce397a5d355008e0642a68a2fc49c
devanshupatil <devanshupatil692@gmail.com>
2025-03-11 14:42:53 +0530

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
-      '/api': 'http://localhost:4100',
+      '/api': 'https://sagar-shop.onrender.com',
     },
   },
 })
